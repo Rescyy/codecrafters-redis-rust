6027b169cb7dba0d88db63e7f5d3b016c1f333af
Crucerescu Vladislav <vladislav.crucerescu@quasar-development.com>
2024-06-17 17:59:09 +0500

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::Read;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,12 +14,18 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_client(mut stream: TcpStream) { 
+    stream.flush().expect("Error flushing the stream");
+    let response = "+PONG\r\n".as_bytes();
+    stream.write_all(response).expect("Failed to send PONG response");
 }
