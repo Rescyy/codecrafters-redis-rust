e21b13c8986f23b102bc15ca7cdb78f19dc790ab
Crucerescu Vladislav <vladislav.crucerescu@quasar-development.com>
2024-06-17 19:04:19 +0500

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::Read;
-use std::io::Write;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        dbg!(&stream);
+    loop {
+        let stream = listener.accept().await;
+        
         match stream {
-            Ok(stream) => {
-                handle_client(stream);
+            Ok((mut stream, _)) => {
+                println!("accepted new connection!");
+
+                tokio::spawn(async move {
+                    let mut buf = [0u8; 512];
+                    loop {
+                        let read_count = stream.read(&mut buf).await.unwrap();
+                        if read_count == 0 {
+                            break;
+                        }
+
+                        stream.write(b"+PONG\r\n").await.unwrap();
+                    }
+                });
             }
             Err(e) => {
-                println!("error: {}", e);
+
             }
         }
     }
@@ -27,14 +39,14 @@ fn main() {
     println!("Program ended");
 }
 
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0u8; 512]; 
-    let response = "+PONG\r\n".as_bytes();
-    loop {
-        let read_bytes = stream.read(&mut buffer).expect("Failed to read from client");
-        if read_bytes == 0 {
-            return;
-        }
-        stream.write_all(response).expect("Failed to send PONG response");
-    }
-}
+// fn handle_client(mut stream: TcpStream) {
+//     let mut buffer = [0u8; 512]; 
+//     let response = "+PONG\r\n".as_bytes();
+//     loop {
+//         let read_bytes = stream.read(&mut buffer).expect("Failed to read from client");
+//         if read_bytes == 0 {
+//             return;
+//         }
+//         stream.write_all(response).expect("Failed to send PONG response");
+//     }
+// }
