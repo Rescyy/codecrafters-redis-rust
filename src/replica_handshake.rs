a3d8c89f14636d880da6e7b8861b1e06e85509cc
Crucerescu Vladislav <vladislav.crucerescu@quasar-development.com>
2024-06-20 17:26:18 +0500

    pass stage

--- src/replica_handshake.rs
+++ src/replica_handshake.rs
@@ -1,4 +1,4 @@
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream};
 
 use crate::{serialize, RespDatatype};
 
@@ -8,8 +8,10 @@ lazy_static! {
 
 pub async fn send_handshake(master_host: &String, master_port: &String, slave_port: &String) -> Result<(), std::io::Error> {
     let mut stream = TcpStream::connect(format!("{master_host}:{master_port}")).await?;
+    let mut buf = Vec::<u8>::new();
     stream.write_all(&PING_COMMAND[..]).await?;
-    stream.flush().await?;
+    stream.read_buf(&mut buf).await?;
+
     let replconf_command1 = serialize(
         &RespDatatype::Array(
             vec![
@@ -20,7 +22,8 @@ pub async fn send_handshake(master_host: &String, master_port: &String, slave_po
         )
     );
     stream.write_all(&replconf_command1).await?;
-    stream.flush().await?;
+    // stream.flush().await?;
+    stream.read_buf(&mut buf).await?;
     let replconf_command2 = serialize(
         &RespDatatype::Array(
             vec![
@@ -31,6 +34,7 @@ pub async fn send_handshake(master_host: &String, master_port: &String, slave_po
         )
     );
     stream.write_all(&replconf_command2).await?;
-    stream.flush().await?;
+    // stream.flush().await?;
+    stream.read_buf(&mut buf).await?;
     return Ok(());
 }
