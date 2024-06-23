2b224e9830280230397f0d9ec7ee8cb84887b351
Crucerescu Vladislav <vladislav.crucerescu@quasar-development.com>
2024-06-24 00:35:25 +0500

    pass stage

--- src/replica_handshake.rs
+++ src/replica_handshake.rs
@@ -13,7 +13,7 @@ pub async fn send_handshake(master_host: &String, master_port: &String, slave_po
     let mut resp_stream_handler = RespStreamHandler::new(stream);
     let mut buf: Vec<u8> = Vec::new();
     resp_stream_handler.write_all(&PING_COMMAND[..]).await?;
-    resp_stream_handler.refill(0).await?;
+    resp_stream_handler.refill(3).await?;
     println!("Passed");
     // resp_stream_handler.write_all(&PING_COMMAND[..]).await?;
     
